a1dd81b421718bc086ee1c65d841c43732c14d04
ArturoZAC <arturoyz2105@gmail.com>
2026-08-18 20:25:25 -0500

    feat: implement Pokemon game components and styles

---
+++ src/modules/pokemon/pages/PokemonGame.vue
@@ -0,0 +1,19 @@
+<template>
+  <section v-if="false" class="flex flex-col justify-center items-center w-screen h-screen">
+    <h1 class="text-3xl">Espere por favor</h1>
+    <h3 class="animate-pulse">Cargando pokemones</h3>
+  </section>
+
+  <section class="flex flex-col justify-center items-center w-screen h-screen">
+    <h1 class="mb-10">Quien es este pokemon?</h1>
+
+    <PokemonPicture />
+
+    <PokemonOptions />
+  </section>
+</template>
+
+<script setup lang="ts">
+  import PokemonOptions from "../components/PokemonOptions.vue"
+  import PokemonPicture from "../components/PokemonPicture.vue"
+</script>
